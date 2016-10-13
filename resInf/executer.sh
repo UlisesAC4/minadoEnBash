c75f81d9237129ba4120209c2bfcafa5c76bb170
Ulises Avila <ulisesavila04@gmail.com>
2016-10-13 12:37:24 -0500

    Commit listo para espanol 2

---
+++ resInf/executer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for i in `seq 1 245`;
+do
+  start=$(cat inicio.txt)
+  end=$(cat final.txt)
+  sep="-"
+  ext=".csv"
+  python2 Exporter.py --since $start --until $end --querysearch 
+'influenza' --maxtweets 3000
+  python dateChange.py
+  echo $start$sep$end$ext
+done
