3b0e2a056d124a47bf71460402a47349e43ba337
Ulises Avila <ulisesavila04@gmail.com>
2016-10-14 02:56:23 +0000

    datos espanoles agregados

--- resInf/executer.sh
+++ resInf/executer.sh
@@ -5,8 +5,7 @@ do
   end=$(cat final.txt)
   sep="-"
   ext=".csv"
-  python2 Exporter.py --since $start --until $end --querysearch 
-'influenza' --maxtweets 3000
+  python2 Exporter.py --since $start --until $end --querysearch 'influenza' --maxtweets 3000
   python dateChange.py
   echo $start$sep$end$ext
 done
